a36437480ff62d4a88a1511a22c1912be37540a6
Phil Freeman <paf31@cantab.net>
2017-07-15 18:17:25 -0700

    eslint

--- src/Data/Record.js
+++ src/Data/Record.js
@@ -2,15 +2,15 @@
 
 exports.unsafeGet = function(label, rec) {
   return rec[label];
-}
+};
 
 exports.unsafeSet = function(label, value, rec) {
   var copy = {};
   for (var key in rec) {
-      if ({}.hasOwnProperty.call(rec, key)) {
-          copy[key] = rec[key];
-      };
-  };
+    if ({}.hasOwnProperty.call(rec, key)) {
+      copy[key] = rec[key];
+    }
+  }
   copy[label] = value;
   return copy;
-}
+};
