b9c514b48fb0e508cb5e6c6c323a3ef885662a1f
Phil Freeman <paf31@cantab.net>
2017-09-03 14:45:46 -0700

    Make eslint happy

--- src/Data/Record/Builder.js
+++ src/Data/Record/Builder.js
@@ -29,14 +29,14 @@ exports.unsafeDelete = function(l) {
 exports.unsafeMerge = function(r1) {
   return function(r2) {
     var copy = {};
-    for (var key in r2) {
-      if ({}.hasOwnProperty.call(r2, key)) {
-        copy[key] = r2[key];
+    for (var k1 in r2) {
+      if ({}.hasOwnProperty.call(r2, k1)) {
+        copy[k1] = r2[k1];
       }
     }
-    for (var key in r1) {
-      if ({}.hasOwnProperty.call(r1, key)) {
-        copy[key] = r1[key];
+    for (var k2 in r1) {
+      if ({}.hasOwnProperty.call(r1, k2)) {
+        copy[k2] = r1[k2];
       }
     }
     return copy;
