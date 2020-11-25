e9af942e94fe663b54a1433c8dce74ea7f40450f
Cyril <sobierajewicz.cyril@gmail.com>
2020-11-25 07:27:48 +0100

    Replace monomorphic proxies by Type.Proxy.Proxy and polymorphic variables (#67)

--- src/Record/ST.purs
+++ src/Record/ST.purs
@@ -10,7 +10,7 @@ module Record.ST
 import Prelude
 
 import Control.Monad.ST (ST, Region)
-import Data.Symbol (class IsSymbol, SProxy, reflectSymbol)
+import Data.Symbol (class IsSymbol, reflectSymbol)
 import Prim.Row as Row
 
 -- | A value of type `STRecord h r` represents a mutable record with fields `r`,
@@ -36,10 +36,10 @@ foreign import unsafePeek
 -- | Read the current value of a field in a mutable record, by providing a
 -- | type-level representative for the label which should be read.
 peek
-  :: forall l h a r r1
+  :: forall proxy l h a r r1
    . Row.Cons l a r1 r
   => IsSymbol l
-  => SProxy l
+  => proxy l
   -> STRecord h r
   -> ST h a
 peek l = unsafePeek (reflectSymbol l)
@@ -54,10 +54,10 @@ foreign import unsafePoke
 -- | Modify a record in place, by providing a type-level representative for the label
 -- | which should be updated.
 poke
-  :: forall l h a r r1
+  :: forall proxy l h a r r1
    . Row.Cons l a r1 r
   => IsSymbol l
-  => SProxy l
+  => proxy l
   -> a
   -> STRecord h r
   -> ST h Unit
@@ -74,10 +74,10 @@ foreign import unsafeModify
 -- | by providing a type-level representative for the label to update
 -- | and a function to update it.
 modify
-  :: forall l h a r r1
+  :: forall proxy l h a r r1
    . Row.Cons l a r1 r
   => IsSymbol l
-  => SProxy l
+  => proxy l
   -> (a -> a)
   -> STRecord h r
   -> ST h Unit
